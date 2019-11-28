999585baa51c15497641110867740940360df3ed
huhn <sehe89@gmail.com>
2019-11-28 16:04:47 +0100

    add eslint and prettier

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const options = require('./lib/options');
-const inquirer  = require('./lib/inquirer');
+const chalk = require('chalk')
+const clear = require('clear')
+const figlet = require('figlet')
+const options = require('./lib/options')
+const inquirer = require('./lib/inquirer')
 
-clear();
+clear()
 
 console.log(
-  chalk.yellow(
-    figlet.textSync('MAM-Module', { horizontalLayout: 'full' })
-  )
-);
-
+    chalk.yellow(figlet.textSync('MAM-Module', { horizontalLayout: 'full' }))
+)
 
 if (options.fileExists('options.js')) {
-    console.log(chalk.red('Already a options file existing!'));
-    process.exit();
+    console.log(chalk.red('Already a options file existing!'))
+    process.exit()
 } else {
-    console.log(chalk.green('Creating options file...'));
+    console.log(chalk.green('Creating options file...'))
 
-    console.log(chalk.greenBright('Options file created.'));  
+    console.log(chalk.greenBright('Options file created.'))
 }
 
 const run = async () => {
-    const _options = await inquirer.askForOptions();
-    console.log(_options);
-  };
-  
-  run();
+    const _options = await inquirer.askForOptions()
+    console.log(_options)
+}
+
+run()
+console.log(chalk.greenBright('"MAM-Module started.'))
