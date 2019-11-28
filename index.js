be948645387acf22ddc6bfa15ff7073c21bab821
huhn <sehe89@gmail.com>
2019-11-28 16:37:04 +0100

    create options file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
-const options = require('./lib/options')
+const Options = require('./lib/options')
 const inquirer = require('./lib/inquirer')
 
 clear()
@@ -9,19 +9,22 @@ clear()
 console.log(
     chalk.yellow(figlet.textSync('MAM-Module', { horizontalLayout: 'full' }))
 )
+const runOptionsQuestions = async () => {
+    const _options = await inquirer.askForOptions()
+    console.log(_options)
+    let x = Options.createFile(_options)
+    console.log('x', x)
+    if (x) {
+        console.log(chalk.greenBright('Options file created.'))
+    } else {
+        console.log(chalk.redBright('Options file not created.'))
+    }
+}
 
-if (options.fileExists('options.js')) {
-    console.log(chalk.red('Already a options file existing!'))
+if (Options.fileExists()) {
+    console.log(chalk.yellow('Already a options file existing.'))
     process.exit()
 } else {
     console.log(chalk.green('Creating options file...'))
-
-    console.log(chalk.greenBright('Options file created.'))
+    runOptionsQuestions()
 }
-
-const run = async () => {
-    const _options = await inquirer.askForOptions()
-    console.log(_options)
-}
-
-run()
