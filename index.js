6f5b0eded4a283a8366f9a1b036aa07719fe0817
huhn <sehe89@gmail.com>
2019-11-28 15:17:44 +0100

    add cli mode and ask for seed

---
+++ index.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const options = require('./lib/options');
+const inquirer  = require('./lib/inquirer');
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('MAM-Module', { horizontalLayout: 'full' })
+  )
+);
+
+
+if (options.fileExists('options.js')) {
+    console.log(chalk.red('Already a options file existing!'));
+    process.exit();
+} else {
+    console.log(chalk.green('Creating options file...'));
+
+    console.log(chalk.greenBright('Options file created.'));  
+}
+
+const run = async () => {
+    const _options = await inquirer.askForOptions();
+    console.log(_options);
+  };
+  
+  run();
